074473df92a894d3f8cc3588635728cb72b42f7d
22045504 <22045504@Briyaniii>
2023-08-07 08:24:21 +0800

    Push for grp

--- C206_CaseStudy/src/Homepage/Group.java
+++ C206_CaseStudy/src/Homepage/Group.java
@@ -78,56 +78,57 @@ public class Group {
 
 	public static void viewAllGroup(ArrayList<Group> groupList) {
 
-		while (true) {
-			Helper.line(50,"-");
-			System.out.println("Menu");
-			Helper.line(50,"-");
-			System.out.println("1. View All Groups");
-			System.out.println("2. View a Specific Group");
-			System.out.println("3. Exit");
+	    while (true) {
+	        Helper.line(40, "=");
+	        System.out.println("Menu");
+	        Helper.line(40, "=");
+	        System.out.println("1. View All Groups");
+	        System.out.println("2. View a Specific Group");
+	        System.out.println("3. Exit");
 
-			int choice = Helper.readInt("Enter your choice:");
+	        int choice = Helper.readInt("Enter your choice:");
 
-			if (choice == 1) {
-				String output = String.format("%-5s %-10s %-15s %s\n", "ID", "NAME", "PARTICIPANTS", "DESCRIPTION");
+	        if (choice == 1) {
+	            Helper.line(45, "=");
+	            String output = String.format("%-5s %-10s %-15s %s\n", "ID", "NAME", "PARTICIPANTS", "DESCRIPTION");
 
-				for (Group group : groupList) {
-					output += String.format("%-5s %-10s %-15d %s\n", group.getId(), group.getName(),
-							group.getParticipants(), group.getDescription());
-				}
+	            for (Group group : groupList) {
+	                output += String.format("%-5s %-10s %-15d %s\n", group.getId(), group.getName(),
+	                        group.getParticipants(), group.getDescription());
+	            }
 
-				System.out.println(output);
+	            System.out.println(output);
 
-			}
+	        } else if (choice == 2) {
+	            String groupName = Helper.readString("\nPlease enter the name of the group to view:\n");
 
-			else if (choice == 2) {
-				String groupName = Helper.readString("\nPlease enter the name of the group to view:\n");
+	            boolean foundGroup = false;
+	            for (Group group : groupList) {
+	                if (group.getName().equalsIgnoreCase(groupName)) {
+	                    foundGroup = true;
+	                    Helper.line(40, "=");
+	                    System.out.println("Group Details");
+	                    Helper.line(40, "=");
+	                    System.out.println("ID: " + group.getId());
+	                    System.out.println("Name: " + group.getName());
+	                    System.out.println("Participants: " + group.getParticipants());
+	                    System.out.println("Description: " + group.getDescription());
+	                }
+	            }
 
-				boolean foundGroup = false;
-				for (Group group : groupList) {
-					if (group.getName().equalsIgnoreCase(groupName)) {
-						foundGroup = true;
-						Helper.line(60, "=");
-						System.out.println("Group Details");
-						Helper.line(60, "=");
-						System.out.println("ID: " + group.getId());
-						System.out.println("Name: " + group.getName());
-						System.out.println("Participants: " + group.getParticipants());
-						System.out.println("Description: " + group.getDescription());
+	            if (!foundGroup) {
+	                System.out.println("Group not found. Please try Again!!");
+	            }
 
-					}
-				}
-
-			} else if (choice == 3) {
-				System.out.println("Goodbye!");
-				break;
-			} else {
-				System.out.println("Invalid choice. Please try again!");
-			}
-
-		}
-
+	        } else if (choice == 3) {
+	            System.out.println("Goodbye!");
+	            break;
+	        } else {
+	            System.out.println("Invalid choice. Please try again!");
+	        }
+	    }
 	}
+
 
 	public static void deleteGroup(ArrayList<Group> groupList) {
 		String idDel = Helper.readString("Enter id of group to delete > ");
